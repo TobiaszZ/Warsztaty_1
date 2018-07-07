113f3c87615318fc906fe6a8b868d95a06f0f777
Tobiasz Ziółkowski <tobiasz.ziolkowski@gmail.com>
2018-07-07 23:35:43 +0200

    kostka do gry

--- src/KostkaDoGry/Kostka.java
+++ src/KostkaDoGry/Kostka.java
@@ -5,51 +5,135 @@ import java.util.Scanner;
 
 public class Kostka {
 
-	public static int dice() {
+	public static int dice() throws NumberFormatException {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Rzuć kostką");
 		String diceThrow = scan.nextLine();
 		scan.close();
-
+		String diceThrowUp = diceThrow.toUpperCase();
 		// cużyć metody cantains + lub contains -
 		String[] splitBySign = new String[2];
 		String splitBySign1 = splitBySign[0];
 		String splitBySign2 = splitBySign[1];
+		int x;
+		int z;
+		int r;
+		int y;
+		int result = 0;
 		if (diceThrow.contains("+")) {
-			
-		}
-
-		if (diceThrow.charAt(3) == '+') {
-			splitBySign = diceThrow.split("\\+");
+			splitBySign = diceThrowUp.split("\\+");
 			splitBySign1 = splitBySign[0];
 			splitBySign2 = splitBySign[1];
-		} else if (diceThrow.charAt(3) == '-') {
+			char firstChar = splitBySign1.charAt(0);
+
+			if (Character.isDigit(firstChar)) {
+				String[] splitByD = new String[2];
+				splitByD = splitBySign1.split("D");
+				String splitByD1 = splitByD[0];
+				String splitByD2 = splitByD[1];
+
+				x = Integer.parseInt(splitByD1);
+				z = Integer.parseInt(splitBySign2);
+				r = Integer.parseInt(splitByD2);
+
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+
+				result = x * y + z;
+			} else {
+				String[] splitByD = new String[2];
+				splitByD = splitBySign1.split("D");
+				String splitByD1 = splitByD[0];
+				String splitByD2 = splitByD[1];
+				z = Integer.parseInt(splitBySign2);
+				r = Integer.parseInt(splitByD2);
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+
+				result = y + z;
+
+			}
+			String[] splitByD = new String[2];
+			splitByD = splitBySign1.split("D");
+			String splitByD1 = splitByD[0];
+			String splitByD2 = splitByD[1];
+
+			x = Integer.parseInt(splitByD1);
+			z = Integer.parseInt(splitBySign2);
+			r = Integer.parseInt(splitByD2);
+
+			Random diceType = new Random();
+			y = diceType.nextInt(r) + 1;
+
+			result = x * y + z;
+		} else if (diceThrowUp.contains("-")) {
 			splitBySign = diceThrow.split("-");
 			splitBySign1 = splitBySign[0];
 			splitBySign2 = splitBySign[1];
+			char firstChar = splitBySign1.charAt(0);
+
+			if (Character.isDigit(firstChar)) {
+				String[] splitByD = new String[2];
+				splitByD = splitBySign1.split("D");
+				String splitByD1 = splitByD[0];
+				String splitByD2 = splitByD[1];
+
+				x = Integer.parseInt(splitByD1);
+				z = Integer.parseInt(splitBySign2);
+				r = Integer.parseInt(splitByD2);
+
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+
+				result = x * y - z;
+			} else {
+				String[] splitByD = new String[2];
+				splitByD = splitBySign1.split("D");
+				String splitByD1 = splitByD[0];
+				String splitByD2 = splitByD[1];
+				z = Integer.parseInt(splitBySign2);
+				r = Integer.parseInt(splitByD2);
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+
+				result = y - z;
+			}
+
 		} else {
-			splitBySign1 = diceThrow;
-			splitBySign2 = "";
-		}
-		String[] splitByD = new String[2];
-		splitByD = splitBySign1.split("D");
-		String splitByD1 = splitByD[0];
-		String splitByD2 = splitByD[1];
+			char firstChar = diceThrow.charAt(0);
+			if (Character.isDigit(firstChar)) {
+				splitBySign1 = diceThrow;
+				// splitBySign2 = "";
 
-		int x = Integer.parseInt(splitByD1);
-		int z = Integer.parseInt(splitBySign2);
-		int r = Integer.parseInt(splitByD2);
+				String[] splitByD = new String[2];
+				splitByD = splitBySign1.split("D");
+				String splitByD1 = splitByD[0];
+				String splitByD2 = splitByD[1];
 
-		Random diceType = new Random();
-		int y = diceType.nextInt(r) + 1;
+				x = Integer.parseInt(splitByD1);
 
-		int result = 0;
-		if (diceThrow.charAt(3) == '+') {
-			result = x * y + z;
-		} else if (diceThrow.charAt(3) == '-') {
-			result = x * y - z;
-		} else {
-			result = x * y;
+				r = Integer.parseInt(splitByD2);
+
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+
+				result = x * y;
+			} else {
+				String letters = "";
+				for (int i = 1; i < diceThrow.length(); i++) {
+					char letter = diceThrow.charAt(i);
+					String letterToString = Character.toString(letter);
+					letters = letters.concat(letterToString);
+
+				}
+
+				r = Integer.parseInt(letters);
+
+				Random diceType = new Random();
+				y = diceType.nextInt(r) + 1;
+				result = y;
+			}
+
 		}
 
 		return result;
@@ -61,10 +145,9 @@ public class Kostka {
 		try {
 			result = dice();
 			System.out.println("Wynik rzutu: " + result);
-		} catch (IndexOutOfBoundsException e) {
-			System.err.println("dadsdasf");
+		} catch (NumberFormatException e) {
+			System.err.println("Niepoprawny kod kostki!");
 		}
-		
 
 	}
 
